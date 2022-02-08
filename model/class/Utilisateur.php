982bc3c317df12c4b37bc2913ee2d7ece5f75c66
Alice Richardeau <82211729+alicerichardeau@users.noreply.github.com>
2022-02-08 09:56:53 +0100

    Update Utilisateur.php

--- model/class/Utilisateur.php
+++ model/class/Utilisateur.php
@@ -1,34 +1,34 @@
 <?php
-class Utilisateur{
+class Utilisateur {
 
     private $id;
     private $username;
     private $password;
 
-    public function __construct($username, $password, $id = null){
+    public function __construct($username, $password, $id = null) {
         $this->username = $username;
         $this->password = $password;
         $this->id = $id;
     }
 //ID
-    public function getId(){
+    public function getId() {
         return $this->id;
     }
-    public function setId($id){
+    public function setId($id) {
         $this->id = $id;
     }
 //USERNAME
-    public function getUsername(){
+    public function getUsername() {
         return $this->username;
     }
-    public function setUsername($username){
+    public function setUsername($username) {
         $this->username = $username;
     }
 //PASSWORD
-    public function getPassword(){
+    public function getPassword() {
         return $this->password;
     }
-    public function setPassword($password){
+    public function setPassword($password) {
         $this->password = $password;
     }
 }
