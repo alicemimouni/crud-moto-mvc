b5d32f0f487364a445c92eaa4c3fc218f1914130
Alice Richardeau <82211729+alicerichardeau@users.noreply.github.com>
2022-02-08 09:59:38 +0100

    Update Moto.php

--- model/class/Moto.php
+++ model/class/Moto.php
@@ -1,24 +1,29 @@
 <?php
-    class Moto{
+
+    class Moto {
+        
         private $idMoto;
         private $brand;
         private $modele;
         private $image;
         private $type;
-        public function __construct($brand, $modele, $image, $type, $idMoto = null){
+        
+        public function __construct($brand, $modele, $image, $type, $idMoto = null) {
             $this->brand = $brand;
             $this->modele = $modele;
             $this->image = $image;
             $this->type = $type;
             $this->idMoto = $idMoto;
         }
+        
 //MOTO
-        public function getIdMoto(){
+        public function getIdMoto() {
                 return $this->idMoto;
         }
-        public function setIdMoto($idMoto){
+        public function setIdMoto($idMoto) {
                 $this->idMoto = $idMoto;
         }
+        
 //BRAND
         public function getBrand() {
             return $this->brand;
@@ -26,13 +31,15 @@
         public function setBrand($brand) {
             $this->brand  = $brand;
         }
+        
 //MODEL
-        public function getModele(){
+        public function getModele() {
             return $this->modele;
         }
-        public function setModele($modele){
+        public function setModele($modele) {
             $this->modele = $modele;
         }
+        
 //IMAGE
         public function getImage() {
             return $this->image;
@@ -40,14 +47,14 @@
         public function setImage($image) {
             $this->image = $image;
         }
+        
 //TYPE
-        public function getType(){
+        public function getType() {
             return $this->type;
         }
-        public function setType($type){
+        public function setType($type) {
             $this->type = $type;
         }
     }
-
 
 ?>
