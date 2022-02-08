da722b00fe07045dd93326c8546560908bca0ab2
Alice Richardeau <82211729+alicerichardeau@users.noreply.github.com>
2022-02-08 10:04:46 +0100

    Update UtilisateurManager.php

--- model/manager/UtilisateurManager.php
+++ model/manager/UtilisateurManager.php
@@ -1,7 +1,8 @@
 <?php
+
 class UtilisateurManager extends Database {
 
-    public function findOneByUsername($username){
+    public function findOneByUsername($username) {
         $request = $this->db->prepare("SELECT * FROM utilisateur WHERE username = :username");
         $request->execute(['username'=>$username]);
         $result = $request->fetch();
